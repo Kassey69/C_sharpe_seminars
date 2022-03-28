e8f1eeea36b9ab2b01ddebd11626462c6df33840
Dmitriy <Kessidy69@gmail.com>
2022-03-28 04:48:05 +0300

    add new Seminar5

--- Seminar5_Array/Seminar5_Task8_differenceArray/Program.cs
+++ Seminar5_Array/Seminar5_Task8_differenceArray/Program.cs
@@ -2,6 +2,47 @@
 Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76
 */
+Console.WriteLine("Введите количество элементов массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+{
+    Console.Write($"Массив заполненный случайными числами = ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100) + new Random().NextDouble();
+
+        Console.Write($"{Math.Round(array[i],2)} ");
+    }
+    Console.WriteLine();
+    double max = 0;
+    double max2 = 0;
+    double min = 0;
+    double result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max2 = max;
+            max = array[i];
+        }
+        else
+        if (array[i] != max && array[i] > max2) max2 = array[i];
+    }
+    Console.WriteLine($"Максимальный элемент = {Math.Round(max,2)} ");
+    min = max2;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] < min)
+        {
+            min = array[j];
+        }
+    }
+    result = max - min;
+    Console.WriteLine($"Минимальный элемент = {Math.Round(min,2)} ");
+    Console.Write($"Разница между элементами массива: {Math.Round(max,2)} - {Math.Round(min,2)} =  {Math.Round(result,2)} ");
+}
+
+/*
 void arrayRandom(int[] array)
 {
     Console.Write($"Массив заполненный случайными числами = ");
@@ -46,3 +87,6 @@ int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 arrayRandom(array);
 differenceArray(array);
+*/
+
+
