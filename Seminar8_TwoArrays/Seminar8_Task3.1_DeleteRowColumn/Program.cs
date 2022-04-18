4d6739b8120a3d3107c8934a62accc5ea3278cd1
Dmitriy <Kessidy69@gmail.com>
2022-04-18 05:52:01 +0300

    add Seminar10

---
+++ Seminar8_TwoArrays/Seminar8_Task3.1_DeleteRowColumn/Program.cs
@@ -0,0 +1,58 @@
+﻿//Удалить ряд где есть 0
+Console.WriteLine("Введите размерность двумерного массива: ");
+Console.Write("Строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] array = new int[n, m];
+for (int g = 0; g < n; g++) //ЗДЕСЬ ВЫДАЕТ ОШИБКУ
+{
+for (int j = 0; j < m; j++)
+{
+array[g, j] = new Random().Next(0, 10);
+Console.Write(array[g, j] + "\t");//Создание двумерного массива
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine();
+int i = 0;
+int f = 0;
+while (i <= n)
+{
+int a = 0;
+for (int j = 0; j < m; j++)
+
+if (array[i, j] == 0)
+
+a = 1;
+if (a == 1) //если в строке есть 0
+{
+f = 1; //фиксируем
+for (int k = 0; k < m; k++)//элементы всех строк
+for (int l = i; l < n - 1; l++)//от последней до следующей за найденной
+array[l, k] = array[l + 1, k];//поднимаем на 1 вверх
+n = n - 1;//уменьшаем количество строк
+}
+else
+i += 1;
+
+}
+if (f == 0)
+Console.Write("НЕТ НУЛЕЙ");
+if (n == 0)
+Console.Write("Все строки удалены");
+else
+{
+Console.WriteLine("Строки с нулями удалены: ");
+for (int z = 0; z < n; z++)//ЗДЕСЬ ВЫДАЕТ ОШИБКУ
+{
+for (int j = 0; j < m; j++)
+{
+Console.Write(array[z, j] + "\t");
+}
+Console.WriteLine();
+}
+}
--- Seminar8_TwoArrays/Seminar8_Task3_DeleteRowColumn/Program.cs
+++ Seminar8_TwoArrays/Seminar8_Task3_DeleteRowColumn/Program.cs
@@ -3,7 +3,7 @@
 //---------------------------------------------------------------------------
 // -> удалить из матрицы все нули и переписать в меньшую матрицу на 1 размер
 //---------------------------------------------------------------------------
-
+/*
 int [,] RandomMatrix(int n, int m)
 {
     int [,] matrix = new int[n,m];
@@ -71,19 +71,16 @@ int[,] matrix = RandomMatrix(n, m);
 Console.WriteLine();
 printCreate(create(matrix));
 
+*/
 
-
-
-
-/*
 Console.WriteLine("Введите количество строк:");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов:");
 int m = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int[n, m];
-for (int i = 0; i < matrix.GetLength(0); i++)
+for (int i = 0; i < n; i++)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int j = 0; j < m; j++)
     {
         matrix[i, j] = new Random().Next(1, 10);
         Console.Write(matrix[i, j] + " ");
@@ -95,39 +92,52 @@ Console.WriteLine();
 // n1 (0.0, 0.1, 0.2, 0.3) n1
 // n2 (1.0, 1.1, 1.2, 1.3) n2
 // n3 (2.0, 2.1, 2.2, 2.3) n3
-
-int minI = 0,
-    minJ = 0,
-    min = matrix[0, 0];
-for (int i = 0; i < matrix.GetLength(0); i++)
+int minIndex = 0,
+min = matrix[0, 0];
+for (int i = 1; i < n; i++)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    if (matrix[i, i] < min)
     {
-        if (matrix[i, j] < min) 
-        {
-            min = matrix[i, j];
-            minI = i;
-            minJ = j;
-        }
+        min = matrix[i, i];
+        minIndex = i;
     }
 }
-//Console.WriteLine(minI + " " + minJ +  " " + min);
-for (int i = 0; i < matrix.GetLength(0); i++)
+for (int i = 0; i < n; i++)
 {
-    matrix[i,minJ] = 0;
+    matrix[i,minIndex] = 0;
+    int save = matrix[i,minIndex];
 }
-    for (int j = 0; j < matrix.GetLength(1); j++)
+ for (int j = 0; j < m; j++)
     {
-        matrix[minI,j] = 0;
+        matrix[minIndex,j] = 0;
     }
 
-
- for (int i = 0; i < matrix.GetLength(0); i++)
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write(matrix[i, j] + " ");
-        }
-        Console.WriteLine();
+        Console.Write(matrix[i, j] + " ");
     }
-*/
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = minIndex; i < n - 1; i++) //находится элемент минимальный по i и при этом  до n-1 до последнего
+    for (int j = 0; j < n; j++) // элемента на строчке будет удаляться вся строчка по столбцам j
+        matrix[i, j] = matrix[i + 1, j]; // поднимаем на 1 строчку вверх
+         
+for (int j = minIndex; j < n - 1; j++) //находится элемент минимальный по j и при этом  до n-1 до последнего
+    for (int i = 0; i < n; i++) // элемента на столбце будет удаляться весь столбец по строчкам i
+        matrix[i, j] = matrix[i, j + 1]; // смещаем на 1 строчку вправо
+     --n; --m; // непосредственно удаляем выделенные выше элементы строчек и столбцов
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
